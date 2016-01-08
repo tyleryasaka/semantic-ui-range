a6cf1e31dce57e7941b108e1232e9e484745aa8f
Tyler Yasaka <contact@tyleryasaka.me>
2016-01-08 06:05:40 -0600

    Implementing module.
    Appears to be at least minimally functional.
    
     Changes to be committed:
    	modified:   range.css
    	modified:   range.js

--- range.js
+++ range.js
@@ -1,5 +1,5 @@
 /*!
- * # Range slider for SemanticUI.
+ * # Range slider for Semantic UI.
  * 
  */
  
@@ -9,109 +9,170 @@
 
 $.fn.range = function(parameters) {
 
-	var offsetCenter = 10; // should be half of the size of the slider thumb
+	var
+		$allModules    = $(this),
+		
+		offsetCenter   = 10 // should be half of the size of the slider thumb
+	;
+	
+  $allModules
+    .each(function() {
+			
+			var
+				settings          = ( $.isPlainObject(parameters) )
+					? $.extend(true, {}, $.fn.range.settings, parameters)
+					: $.extend({}, $.fn.range.settings),
 
-	var determineValue = function(startPos, endPos, currentPos, options) {
-		var ratio = (currentPos - startPos) / (endPos - startPos);
-		var range = options.max - options.min;
-		return Math.round(ratio * range) + options.min;
-	}
+				namespace       = settings.namespace,
+				min             = settings.min,
+				max             = settings.max,
+				start           = settings.start,
+				input           = settings.input,
 
-	var determinePosition = function(range, options) {
-		var ratio = (options.start - options.min) / (options.max - options.min);
-		return Math.round(ratio * range) + options.min;
-	}
+				eventNamespace  = '.' + namespace,
+				moduleNamespace = 'module-' + namespace,
 
-	var setValue = function(options, value) {
-		if(options.input) {
-			$(options.input).val(value);
-		} else {
-			options.callback(value);
-		}
-	}
+				$module         = $(this),
 
-	var setPosition = function(element, value) {
-		$(element).find('.thumb').css({left: String(value) + 'px'});
-		$(element).find('.track-left').css({width: String(value) + 'px'});
-	}
+				element         = this,
+				instance        = $module.data(moduleNamespace),
+				
+				module
+			;
+			
+			module = {
+				
+				initialize: function() {
+					module.instantiate();
+				},
+				
+				instantiate: function() {
+					instance = module;
+					$module
+						.data(moduleNamespace, module)
+					;
+					$(element).html("<div class='inner'><div class='track'></div><div class='track-left'></div><div class='thumb'></div></div>");
+					// set start location
+					var position = module.determinePosition($(element).width() - offsetCenter);
+					module.setPosition(position);
+					module.setValue(settings.start);
+					// event listeners
+					$(element).find('.track, .thumb, .inner').on('mousedown', function(event) {
+						event.stopImmediatePropagation();
+						event.preventDefault();
+						$(this).closest(".range").trigger('mousedown', event);
+					});
+					$(element).find('.track, .thumb, .inner').on('touchstart', function(event) {
+						event.stopImmediatePropagation();
+						event.preventDefault();
+						$(this).closest(".range").trigger('touchstart', event);
+					});
+					$(element).on('mousedown', function(event, originalEvent) {
+						module.rangeMousedown(event, false, originalEvent);
+					});
+					$(range).on('touchstart', function(event, originalEvent) {
+						module.rangeMousedown(event, true, originalEvent);
+					});
+				},
+				
+				determineValue: function(startPos, endPos, currentPos) {
+					var ratio = (currentPos - startPos) / (endPos - startPos);
+					var range = settings.max - settings.min;
+					return Math.round(ratio * range) + settings.min;
+				},
 
-	var rangeMousedown = function(mdEvent, isTouch, options, originalEvent) {
-		mdEvent.preventDefault();
-		var dimensions = mdEvent.target.getBoundingClientRect();
-		var pageX;
-		if(isTouch) {
-			pageX = originalEvent.originalEvent.touches[0].pageX;
-		} else {
-			pageX = (typeof mdEvent.pageX != 'undefined') ? mdEvent.pageX : originalEvent.pageX;
-		}
-		var value = determineValue(dimensions.left, dimensions.right, pageX, options);
-		if(value >= options.min && value <= options.max) {
-			setPosition(mdEvent.target, pageX - dimensions.left - offsetCenter);
-			$(document).css({cursor: 'pointer'});
-			setValue(options, value);
-			var rangeMousemove = function(mmEvent) {
-				mmEvent.preventDefault();
-				if(isTouch) {
-					pageX = mmEvent.originalEvent.touches[0].pageX;
-				} else {
-					pageX = mmEvent.pageX;
-				}
-				value = determineValue(dimensions.left, dimensions.right, pageX, options);
-				if( pageX >= dimensions.left && pageX <= dimensions.right) {
-					if(value >= options.min && value <= options.max) {
-						setPosition(mdEvent.target, pageX - dimensions.left - offsetCenter);
-						setValue(options, value);
+				determinePosition: function(range) {
+					var ratio = (settings.start - settings.min) / (settings.max - settings.min);
+					return Math.round(ratio * range) + settings.min;
+				},
+
+				setValue: function(value) {
+					if(settings.input) {
+						$(settings.input).val(value);
+					} else {
+						settings.onChange(value);
+					}
+				},
+
+				setPosition: function(value) {
+					$(element).find('.thumb').css({left: String(value) + 'px'});
+					$(element).find('.track-left').css({width: String(value) + 'px'});
+				},
+
+				rangeMousedown: function(mdEvent, isTouch, originalEvent) {
+					mdEvent.preventDefault();
+					var dimensions = mdEvent.target.getBoundingClientRect();
+					var pageX;
+					if(isTouch) {
+						pageX = originalEvent.originalEvent.touches[0].pageX;
+					} else {
+						pageX = (typeof mdEvent.pageX != 'undefined') ? mdEvent.pageX : originalEvent.pageX;
+					}
+					var value = module.determineValue(dimensions.left, dimensions.right, pageX);
+					if(value >= settings.min && value <= settings.max) {
+						module.setPosition(pageX - dimensions.left - offsetCenter);
+						$(document).css({cursor: 'pointer'});
+						module.setValue(value);
+						var rangeMousemove = function(mmEvent) {
+							mmEvent.preventDefault();
+							if(isTouch) {
+								pageX = mmEvent.originalEvent.touches[0].pageX;
+							} else {
+								pageX = mmEvent.pageX;
+							}
+							value = module.determineValue(dimensions.left, dimensions.right, pageX);
+							if( pageX >= dimensions.left && pageX <= dimensions.right) {
+								if(value >= settings.min && value <= settings.max) {
+									module.setPosition(pageX - dimensions.left - offsetCenter);
+									module.setValue(value);
+								}
+							}
+						}
+						var rangeMouseup = function(muEvent) {
+							$(document).css({cursor: 'auto'});
+							if(isTouch) {
+								$(document).off('touchmove', rangeMousemove);
+								$(document).off('touchend', rangeMouseup);
+							} else {
+								$(document).off('mousemove', rangeMousemove);
+								$(document).off('mouseup', rangeMouseup);
+							}
+						}
+						if(isTouch) {
+							$(document).on('touchmove', rangeMousemove);
+							$(document).on('touchend', rangeMouseup);
+						}
+						else {
+							$(document).on('mousemove', rangeMousemove);
+							$(document).on('mouseup', rangeMouseup);
+						}
 					}
 				}
-			}
-			var rangeMouseup = function(muEvent) {
-				$(document).css({cursor: 'auto'});
-				if(isTouch) {
-					$(document).off('touchmove', rangeMousemove);
-					$(document).off('touchend', rangeMouseup);
-				} else {
-					$(document).off('mousemove', rangeMousemove);
-					$(document).off('mouseup', rangeMouseup);
-				}
-			}
-			if(isTouch) {
-				$(document).on('touchmove', rangeMousemove);
-				$(document).on('touchend', rangeMouseup);
-			}
-			else {
-				$(document).on('mousemove', rangeMousemove);
-				$(document).on('mouseup', rangeMouseup);
-			}
-		}
-	}
+			
+			};
 
-	var initialize = function(options) {
-		var range = options.range;
-		delete options.range;
-		$(range).html("<div class='inner'><div class='track'></div><div class='track-left'></div><div class='thumb'></div></div>");
-		if(typeof options.start != 'undefined') {
-			var position = determinePosition($(range).width() - offsetCenter, options);
-			setPosition(range, position);
-			setValue(options, options.start);
-		}
-		$(range).find('.track, .thumb, .inner').on('mousedown', function(event) {
-			event.stopImmediatePropagation();
-			event.preventDefault();
-			$(this).closest(".range").trigger('mousedown', event);
-		});
-		$(range).find('.track, .thumb, .inner').on('touchstart', function(event) {
-			event.stopImmediatePropagation();
-			event.preventDefault();
-			$(this).closest(".range").trigger('touchstart', event);
-		});
-		$(range).on('mousedown', function(event, originalEvent) {
-			rangeMousedown(event, false, options, originalEvent);
-		});
-		$(range).on('touchstart', function(event, originalEvent) {
-			rangeMousedown(event, true, options, originalEvent);
-		});
-	}
+			module.initialize();
+			
+    })
+  ;
+  
+  return this;
 
 };
+
+$.fn.range.settings = {
+
+  name         : 'Range',
+  namespace    : 'range',
+
+	min          : 0,
+	max          : false,
+	start        : 0,
+	input        : false,
+	
+	onChange     : function(value){},
+
+};
+
 
 })( jQuery, window, document );
